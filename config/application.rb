8faecd4bebd80f8a6bd16dfb1cd97794034f4e39
ckashby <clay.ashby@gmail.com>
2018-06-07 15:39:38 -0700

    Renamed app and created new databases

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Mh2
+module Mh3
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
