7bac5955f1259fd4aca4394588e5c43729269622
ckashby <clay.ashby@gmail.com>
2018-06-09 17:11:13 -0700

    Fixed test/integration/site_layout_test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -4,4 +4,12 @@ class SiteLayoutTestTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "Site Layout Test" do 
+  	get root_path
+  	assert_template 'pages/index'
+  	assert_select "a[href=?]", root_path, count: 1
+  	assert_select "a[href=?]", about_path, count: 2
+  	assert_select "a[href=?]", contact_path, count:2
+
+  end
 end
